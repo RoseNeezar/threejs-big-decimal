62badcaf34a1597efc1d5f5cbe1476c8ba54ce82
Neezar <rforlyffe@gmaicl.com>
2022-04-23 23:27:19 +0800

    add react 3 fiber init with vanilla 3js

--- packages/basics/src/script.js
+++ packages/basics/src/script.js
@@ -1,40 +1,72 @@
-import './style.css'
-import * as THREE from 'three'
+import "./style.css";
+import * as THREE from "three";
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Object
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+ */ f;
+const group = new THREE.Group();
+group.scale.set(1, 2);
+scene.add(group);
 
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xffff00 })
+);
+group.add(cube2);
+
+cube2.position.x = -2;
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
+// mesh.position.z = 0.5;
+// mesh.position.y = 0.5;
+// mesh.position.x = 0.7;
+
+// mesh.rotation.reorder("XYZ");
+// mesh.rotation.y = 3.14159 * 2;
+// mesh.rotation.x = 3.14159 * 0.32;
+
+// mesh.scale.set(2, 0.5, 0.5);
+
+// scene.add(mesh);
+
+const axesHelper = new THREE.AxesHelper();
+
+scene.add(axesHelper);
 /**
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600,
+};
 
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// camera.lookAt(mesh.position);
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
